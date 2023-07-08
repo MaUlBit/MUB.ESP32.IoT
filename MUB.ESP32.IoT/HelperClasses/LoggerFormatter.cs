40b9a89c9d89847d548583a603d58774bb327fab
bittnermartin <bittnermartin@outlook.de>
2023-07-08 22:35:32 +0200

    - basic set-up with blinking LED and Logging to start from

---
+++ MUB.ESP32.IoT/HelperClasses/LoggerFormatter.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace MUB.ESP32.IoT
+{
+    public class LoggerFormatter
+    {
+        public string MessageFormatterStatic(string className, LogLevel logLevel, EventId eventId, string state, Exception exception)
+        {
+            string logstr = string.Empty;
+            switch (logLevel)
+            {
+                case LogLevel.Trace:
+                    logstr = "TRACE: ";
+                    break;
+                case LogLevel.Debug:
+                    logstr = "DEBUG: ";
+                    break;
+                case LogLevel.Warning:
+                    logstr = "WARNING: ";
+                    break;
+                case LogLevel.Error:
+                    logstr = "ERROR: ";
+                    break;
+                case LogLevel.Critical:
+                    logstr = "CRITICAL:";
+                    break;
+                case LogLevel.None:
+                case LogLevel.Information:
+                default:
+                    break;
+            }
+
+            string eventstr = eventId.Id != 0 ? $" Event ID:{eventId}, " : string.Empty;
+            //string msg = $"[{className}]{eventstr}{logstr}{state}";
+            string date = DateTime.UtcNow.AddHours(2).ToString("HH:mm:ss");
+            string msg = $"[{className}] {date} {eventstr}{state}";
+
+            if (exception != null)
+            {
+                msg += $" {exception}";
+            }
+
+            return msg;
+        }
+    }
+}
--- MUB.ESP32.IoT/Program.cs
+++ MUB.ESP32.IoT/Program.cs
@@ -1,20 +1,41 @@
+using Microsoft.Extensions.Logging;
+using nanoFramework.Logging.Debug;
+using nanoFramework.Logging;
 using System;
 using System.Diagnostics;
 using System.Threading;
+using System.Device.Gpio;
 
 namespace MUB.ESP32.IoT
 {
     public class Program
     {
+        // Logger
+        private static ILogger _logger;
+
+        // Logger Formatter
+        private static LoggerFormatter loggerFormatter = new LoggerFormatter();
+
         public static void Main()
         {
-            Debug.WriteLine("Hello from nanoFramework!");
+            // Configure the logger
+            LogDispatcher.LoggerFactory = new DebugLoggerFactory();
+            _logger = LogDispatcher.LoggerFactory.CreateLogger("Debug");
+            LoggerExtensions.MessageFormatter = typeof(LoggerFormatter).GetType().GetMethod("MessageFormatterStatic");
+            _logger.LogDebug("Starting Device ...");
 
-            Thread.Sleep(Timeout.Infinite);
+            // Initialize GPIO controller and LED pin
+            var gpioController = new GpioController();
+            var ledPin = gpioController.OpenPin(13, PinMode.Output);
 
-            // Browse our samples repository: https://github.com/nanoframework/samples
-            // Check our documentation online: https://docs.nanoframework.net/
-            // Join our lively Discord community: https://discord.gg/gCyBu8T
+            while (true)
+            {
+                // Toggle LED on/off to indicate that the controller is running
+                ledPin.Toggle();
+
+                // Delay for 1 second
+                Thread.Sleep(1000);
+            }
         }
     }
 }
